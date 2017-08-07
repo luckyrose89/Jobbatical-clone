5f097f1854359f522eaa1f3acd6c2af954dd911f
Brian C <cbchien46@gmail.com>
2017-08-07 15:24:02 +0800

    add need attributes to job model

--- src/server/models/Job.js
+++ src/server/models/Job.js
@@ -4,11 +4,31 @@ var mongoose = require('mongoose');
 var JobSchema = new mongoose.Schema({
 	job: {
 		name: { type: String, required: true },
+		datePosted: { type: String },
+		hiringOrganization: {
+			name: { type: String },
+			location: {
+	        	addressCity: { type: String },
+	            addressRegion: { type: String },
+	            addressCountry: { type: String }
+        	}
+		},
 	    // Pictures must start with "http://"
 	    pictures: [{ type: String, match: /^http:\/\//i }],
 	    description: { type: String, required: true },
 	    // category: { type: String, required: true }
-	    category: Category.categorySchema
+	    category: Category.categorySchema,
+	    incentiveCompensation: { type: String },
+	    jobLocation: {
+        	addressCity: { type: String },
+            addressRegion: { type: String },
+            addressCountry: { type: String }
+        },
+        employmentType: { type: String }, //part-time or full-times
+        industry: { type: String },
+        responsibilities: { type: String }, 
+        qualifications: { type: String },
+        validThrough: { type: String }
 	}
   });
 
