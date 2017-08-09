8ad8e05dfb3c8c3938dffcc34255a0b466be0ac9
ksmai <kasingmai@gmail.com>
2017-08-09 18:49:09 +0800

    Add some demo job data

---
+++ data/jobs.js
@@ -0,0 +1,490 @@
+module.exports = [
+  {
+    _id: '598ad2b08c04aa9c9d597b4d',
+    name: 'Nurse',
+    datePosted: 1502267722090,
+    hiringOrganization: {
+      name: 'Chingu Hospital',
+      location: {
+        addressCity: 'San Francisco',
+        addressRegion: 'California',
+        addressCountry: 'United States',
+      },
+    },
+    pictures: ['http://pic.chingu-hospital.org/abc.jpg'],
+    description: 'We are looking for full-time nurses to assist in daily health care of chingus. Chingus are rich so there will be attractive compensation packages!',
+    category: {
+      region: 'California',
+      keyword: [
+        'Nursing',
+        'Hospital',
+        'Healthcare',
+      ],
+    },
+    incentiveCompensation: 'monthly salary',
+    jobLocation: {
+      addressCity: 'San Francisco',
+      addressRegion: 'California',
+      addressCountry: 'United States'
+    },
+    employmentType: 'full-time',
+    industry: 'Healthcare',
+    responsibilities: 'Take care of sick Chingus', 
+    qualifications: 'Some nursing certificates',
+    validThrough: '2019-01-01T03:00:00Z',
+  }, {
+    _id: '598ad3079e28f4d08fe330c9',
+    name: 'Javascript programmer',
+    datePosted: 1483117261000,
+    hiringOrganization: {
+      name: 'Evil Employer',
+      location: {
+        addressCity: 'Evil City',
+        addressRegion: 'Evil Region',
+        addressCountry: 'Evil Country',
+      },
+    },
+    pictures: ['http://evil.com/evil.jpg'],
+    description: 'We want capable programmers that know javascript, nodejs, php, angular, react, vue, ruby on rails, java, photoshop, illustrator, perl, c++, rust, elixir and rocket science. We will be offering at least minimum wage of course. We need you to work 7 days a week.',
+    category: {
+      region: 'Evil Region',
+      keyword: [
+        'Programmer',
+        'Tech',
+        'IT',
+      ],
+    },
+    incentiveCompensation: 'monthly salary',
+    jobLocation: {
+      addressCity: 'Evil City',
+      addressRegion: 'Evil Region',
+      addressCountry: 'Evil Country'
+    },
+    employmentType: 'full-time',
+    industry: 'Tech',
+    responsibilities: 'Plan, design, develop, code, test, deploy and maintain 9000 applications and do all the IT support in the company', 
+    qualifications: '10+ years of relevant experience',
+    validThrough: '2018-06-06T06:66:66Z',
+  }, {
+    _id: '598ad3145cf2cb5c1c17a6b0',
+    name: 'Business Analyst',
+    datePosted: 1502121600000,
+    hiringOrganization: {
+      name: 'A big business company',
+      location: {
+        addressCity: 'Big City',
+        addressRegion: 'Big Region',
+        addressCountry: 'Big Country',
+      },
+    },
+    pictures: ['http://some-host.com/some-pic.jpg'],
+    description: 'Manage business requirements, quality control, cost and timeline of our awesome projects',
+    category: {
+      region: 'Big Region',
+      keyword: [
+        'Big',
+        'BigBig',
+        'Business',
+      ],
+    },
+    incentiveCompensation: 'monthly salary',
+    jobLocation: {
+      addressCity: 'Big City',
+      addressRegion: 'Big Region',
+      addressCountry: 'Big Country'
+    },
+    employmentType: 'part-time',
+    industry: 'business',
+    responsibilities: 'Deliver high quality business solutions', 
+    qualifications: 'Bachelor or above',
+    validThrough: '2019-09-11T03:00:00Z',
+  }, {
+    _id: '598ad31f3f7d878cc6b9f2e7',
+    name: 'Part-time research assistant',
+    datePosted: 1506704461000,
+    hiringOrganization: {
+      name: 'Foo University',
+      location: {
+        addressCity: 'Foo City',
+        addressRegion: 'Foo Region',
+        addressCountry: 'Foo Country',
+      },
+    },
+    pictures: ['http://foo.university.edu/pic'],
+    description: 'Professor Bar needs a smart assistant for his project in quantun mechanics!',
+    category: {
+      region: 'Foo Region',
+      keyword: [
+        'Research',
+        'Physics',
+      ],
+    },
+    incentiveCompensation: 'hourly salary',
+    jobLocation: {
+      addressCity: 'Foo City',
+      addressRegion: 'Foo Region',
+      addressCountry: 'Foo Country'
+    },
+    employmentType: 'part-time',
+    industry: 'Research',
+    responsibilities: 'Help doing research obviously', 
+    qualifications: 'PhD in physics or mathematics',
+    validThrough: '2018-12-25T22:01:00Z',
+  }, {
+    _id: '598ad32ae209fbb3b61e4437',
+    name: 'Data Entry Clerk',
+    datePosted: 1509382861000,
+    hiringOrganization: {
+      name: 'Bear Team 8',
+      location: {
+        addressCity: 'Chingu City',
+        addressRegion: 'Chingu Region',
+        addressCountry: 'Chingu Country',
+      },
+    },
+    pictures: ['http://chingu.is/awesome.jpg'],
+    description: 'Create dummy job data quickly to populate the database for development purpose',
+    category: {
+      region: 'Remote',
+      keyword: [
+        'webdev',
+        'chingu',
+        'programming',
+      ],
+    },
+    incentiveCompensation: 'no salary',
+    jobLocation: {
+      addressCity: 'Remote',
+      addressRegion: 'Remote',
+      addressCountry: 'Remote'
+    },
+    employmentType: 'full-time',
+    industry: 'Tech',
+    responsibilities: 'Create 9000 fake data for testing database and related parts of applications', 
+    qualifications: 'Some creative writing skills',
+    validThrough: '2019-10-01T03:00:00Z',
+  }, {
+    _id: '598ad333f427fd077acf4177',
+    name: 'Actuarial Intern',
+    datePosted: 1502267722090,
+    hiringOrganization: {
+      name: 'Money Money',
+      location: {
+        addressCity: 'Hong Kong',
+        addressRegion: 'Hong Kong',
+        addressCountry: 'Hong Kong',
+      },
+    },
+    pictures: ['http://money.money/money.jpg'],
+    description: 'This position assists us in our financial analysis',
+    category: {
+      region: 'Hong Kong',
+      keyword: [
+        '$',
+        '$$',
+        '$$$',
+      ],
+    },
+    incentiveCompensation: 'no salary',
+    jobLocation: {
+      addressCity: 'Hong Kong',
+      addressRegion: 'Hong Kong',
+      addressCountry: 'Hong Kong'
+    },
+    employmentType: 'part-time',
+    industry: 'Finance',
+    responsibilities: 'Perform analysis', 
+    qualifications: 'No prior experience needed',
+    validThrough: '2022-03-24T05:05:55Z',
+  }, {
+    _id: '598ad33af881d6d1cb9247e9',
+    name: 'Surveyor',
+    datePosted: 1493861613000,
+    hiringOrganization: {
+      name: 'XYZ Surveying Company',
+      location: {
+        addressCity: 'Shanghai',
+        addressRegion: 'Shanghai',
+        addressCountry: 'China',
+      },
+    },
+    pictures: ['http://xyzsurvey.org/ourpic'],
+    description: 'Insert some surveying description here',
+    category: {
+      region: 'Shanghai',
+      keyword: [
+        'Surveying',
+      ],
+    },
+    incentiveCompensation: 'monthly salary',
+    jobLocation: {
+      addressCity: 'Beijing',
+      addressRegion: 'China',
+      addressCountry: 'China'
+    },
+    employmentType: 'full-time',
+    industry: 'Surveying',
+    responsibilities: 'Survey', 
+    qualifications: 'Bachelor in Surveying or equivalent',
+    validThrough: '2017-06-26T06:26:26Z',
+  }, {
+    _id: '598ad355c8faf19aacd57f9e',
+    name: 'Bus Driver',
+    datePosted: 1515287349009,
+    hiringOrganization: {
+      name: 'ABC Bus Corp.',
+      location: {
+        addressCity: 'Taipei',
+        addressRegion: 'Taiwan',
+        addressCountry: 'Taiwan',
+      },
+    },
+    pictures: ['http://pic.abc-bus.com/awesomepic.jpg'],
+    description: 'Just some driving',
+    category: {
+      region: 'Taiwan',
+      keyword: [
+        'Bus',
+        'Driving',
+        'Drifting',
+      ],
+    },
+    incentiveCompensation: 'monthly salary',
+    jobLocation: {
+      addressCity: 'Taipei',
+      addressRegion: 'Taiwan',
+      addressCountry: 'Taiwan'
+    },
+    employmentType: 'full-time',
+    industry: 'Transportation',
+    responsibilities: 'Drive the bus', 
+    qualifications: 'Driving license',
+    validThrough: '2018-01-03T00:00:00Z',
+  }, {
+    _id: '598ad35ecbc86d186d9cc512',
+    name: 'Superhero',
+    datePosted: 1502267722090,
+    hiringOrganization: {
+      name: 'Secret Agency',
+      location: {
+        addressCity: 'Secret City',
+        addressRegion: 'Mars',
+        addressCountry: 'Galaxy X776',
+      },
+    },
+    pictures: ['http://secret.alien/secret.jpg'],
+    description: 'Be brave, and save the world. We will provide all necessary training and equipments.',
+    category: {
+      region: 'Earth',
+      keyword: [
+        'Superhero',
+        'Marvel',
+        'DC',
+      ],
+    },
+    incentiveCompensation: 'superpower',
+    jobLocation: {
+      addressCity: 'Any City',
+      addressRegion: 'Any Region',
+      addressCountry: 'All countries on planet Earth'
+    },
+    employmentType: 'full-time',
+    industry: 'World-saving',
+    responsibilities: 'Save the day. Kill that monster', 
+    qualifications: '7 feet tall',
+    validThrough: '2046-07-07T17:32:06Z',
+  }, {
+    _id: '598ad36af30f2d066e83c615',
+    name: 'Baker',
+    datePosted: 1485878400000,
+    hiringOrganization: {
+      name: 'Our baking com',
+      location: {
+        addressCity: 'Bangkok',
+        addressRegion: 'Thailand',
+        addressCountry: 'Thailand',
+      },
+    },
+    pictures: ['http://google.com/'],
+    description: 'Bake something',
+    category: {
+      region: 'Bangkok',
+      keyword: [
+        'food',
+      ],
+    },
+    incentiveCompensation: 'monthly salary',
+    jobLocation: {
+      addressCity: 'Bangkok',
+      addressRegion: 'Thailand',
+      addressCountry: 'Thailand'
+    },
+    employmentType: 'full-time',
+    industry: 'Food',
+    responsibilities: 'Bake some ****', 
+    qualifications: '1 year or more experience',
+    validThrough: '2019-03-01T03:00:00Z',
+  }, {
+    _id: '598ad372f2888a8a4e3771a7',
+    name: 'Police officer',
+    datePosted: 1490976000000,
+    hiringOrganization: {
+      name: 'Police Station',
+      location: {
+        addressCity: 'London',
+        addressRegion: 'United Kingdom',
+        addressCountry: 'United Kingdom',
+      },
+    },
+    pictures: ['http://pic.police.gov.uk/pic'],
+    description: 'Arrest that bad guy',
+    category: {
+      region: 'London',
+      keyword: [
+        'Government',
+        'Police',
+        'Cool',
+      ],
+    },
+    incentiveCompensation: 'monthly salary',
+    jobLocation: {
+      addressCity: 'London',
+      addressRegion: 'London',
+      addressCountry: 'United Kingdom'
+    },
+    employmentType: 'full-time',
+    industry: 'Government',
+    responsibilities: 'Fight terrorists', 
+    qualifications: 'Strong muscles',
+    validThrough: '2018-06-01T03:00:00Z',
+  }, {
+    _id: '598ad37a873bcd391fe19696',
+    name: 'Designer',
+    datePosted: 1491667200000,
+    hiringOrganization: {
+      name: 'Some webdev company',
+      location: {
+        addressCity: 'New Delhi',
+        addressRegion: 'India',
+        addressCountry: 'India',
+      },
+    },
+    pictures: ['http://www.webdev.com/pic/arvavbrb.jpg'],
+    description: 'Our current web pages look like crap drawn by 3-year-old kids. Come and redesign for us!',
+    category: {
+      region: 'New Delhi',
+      keyword: [
+        'design',
+        'webdev',
+        'photoshop',
+      ],
+    },
+    incentiveCompensation: 'monthly salary',
+    jobLocation: {
+      addressCity: 'New Delhi',
+      addressRegion: 'India',
+      addressCountry: 'India'
+    },
+    employmentType: 'full-time',
+    industry: 'Design',
+    responsibilities: 'Design webpages and work closely with our developer team', 
+    qualifications: '3 years of relevant experience',
+    validThrough: '2017-11-13T11:20:20Z',
+  }, {
+    _id: '598ad382157b4e01fb274395',
+    name: 'Retail Store Manager',
+    datePosted: 1457032033000,
+    hiringOrganization: {
+      name: 'Nike',
+      location: {
+        addressCity: 'Tokyo',
+        addressRegion: 'Tokyo',
+        addressCountry: 'Japan',
+      },
+    },
+    pictures: ['http://nike.fake/a.jpg'],
+    description: 'Perform day-to-day operation for our wonderful store',
+    category: {
+      region: 'Tokyo',
+      keyword: [
+        'Retail',
+        'Management',
+        'Nike',
+      ],
+    },
+    incentiveCompensation: 'monthly salary',
+    jobLocation: {
+      addressCity: 'Tokyo',
+      addressRegion: 'Tokyo',
+      addressCountry: 'Japan'
+    },
+    employmentType: 'full-time',
+    industry: 'Retail',
+    responsibilities: 'Sell shoes', 
+    qualifications: 'none',
+    validThrough: '2016-06-01T03:00:00Z',
+  }, {
+    _id: '598ad38a24ac38de5c40a80a',
+    name: 'Astronaut',
+    datePosted: 2445523200000,
+    hiringOrganization: {
+      name: 'Space Program',
+      location: {
+        addressCity: 'Moscow',
+        addressRegion: 'Central',
+        addressCountry: 'Russia',
+      },
+    },
+    pictures: ['http://space.program.ru/a-picture-of-moon.jpg'],
+    description: 'The Earth is dying! We need to send some astronaut to Mars and build our cities there!',
+    category: {
+      region: 'Mars',
+      keyword: [
+        'mars',
+        'space',
+      ],
+    },
+    incentiveCompensation: 'life',
+    jobLocation: {
+      addressCity: 'New City',
+      addressRegion: 'Mars',
+      addressCountry: 'Mankind united'
+    },
+    employmentType: 'full-time',
+    industry: 'Space',
+    responsibilities: 'Drive the spaceship to Mars and start building things there', 
+    qualifications: 'Physically fit',
+    validThrough: '2050-01-01T03:00:00Z',
+  }, {
+    _id: '598ad392f037eaad3ad3b59d',
+    name: 'Lawyer',
+    datePosted: 1570583349000,
+    hiringOrganization: {
+      name: 'International Firm',
+      location: {
+        addressCity: 'Warsaw',
+        addressRegion: 'Poland',
+        addressCountry: 'Poland',
+      },
+    },
+    pictures: ['http://www.somedomain.po/international.jpeg'],
+    description: 'Join our law team today at International Firm!',
+    category: {
+      region: 'Warsaw',
+      keyword: [
+        'Law',
+      ],
+    },
+    incentiveCompensation: 'monthly salary',
+    jobLocation: {
+      addressCity: 'Warsaw',
+      addressRegion: 'Poland',
+      addressCountry: 'Poland'
+    },
+    employmentType: 'full-time',
+    industry: 'Legal',
+    responsibilities: 'Do some legal/illegal stuffs', 
+    qualifications: 'Some law certificates',
+    validThrough: '2019-11-11T11:11:11Z',
+  },
+];
