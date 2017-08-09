2989d628879fcde37e3cd8b4259cbc89e79c3486
Brian C <cbchien46@gmail.com>
2017-08-09 09:30:22 +0800

    job dateposted with deault .now

--- src/server/models/Job.js
+++ src/server/models/Job.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 var JobSchema = new mongoose.Schema({
 	job: {
 		name: { type: String, required: true },
-		datePosted: { type: String },
+		datePosted: { type: Date, default: Date.now },
 		hiringOrganization: {
 			name: { type: String },
 			location: {
