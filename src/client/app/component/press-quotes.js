a8af649288d679bc071eb5901a5dffb7697a9d92
Divya Mathur <avidrutham@gmail.com>
2017-08-22 19:56:30 +0530

    Landing-page additions

---
+++ src/client/app/component/press-quotes.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+
+class PressQuotes extends Component {
+	render() {
+		return (
+			<section className="press-section">
+				<div className="container">
+						<h3 className="header">The Press</h3>
+						<h2 className="header">JobsOnTheGo in News coverage</h2>
+					<div className="row">
+						<div className="col-md-4 col-sm-12">
+							<div className="pressbox">
+								<div className="quote">
+								</div>
+								<p>Need a break from Home but want to keep working? Try JobsOnTheGo</p>
+								<div className="logo">
+								</div>
+							</div>
+						</div>
+						<div className="col-md-4 col-sm-12">
+							<div className="pressbox">
+								<div className="quote">
+								</div>
+								<p>JobsOnTheGo provides a much needed platform in the market.</p>
+								<div className="logo">
+								</div>
+							</div>
+						</div>
+						<div className="col-md-4 col-sm-12">
+							<div className="pressbox">
+								<div className="quote">
+								</div>
+								<p>For your international business requirements get in touch with JobsOnTheGo</p>
+								<div className="logo">
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		)
+	}
+}
+
+export default PressQuotes;
